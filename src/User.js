3f8f24e34f457b17db93456223601e740eb1e13f
Kiko <sthyeon11#gmail.com>
2023-05-27 16:42:29 +0000

    use context api

--- src/User.js
+++ src/User.js
@@ -1,22 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import axios from "axios";
 import { useAsync } from "react-async";
-
-async function getUser({ id }) {
-  const response = await axios.get(
-    `https://jsonplaceholder.typicode.com/users/${id}`
-  );
-  return response.data;
-}
+import { getUser, useUsersDispatch, useUsersState } from "./UserContext";
 
 function User({ id }) {
-  const {
-    data: user,
-    error,
-    isLoading,
-  } = useAsync({ promiseFn: getUser, id, watch: id });
+  const state = useUsersState();
+  const dispatch = useUsersDispatch();
 
-  if (isLoading) return <div>로딩중</div>;
+  useEffect(() => {
+    getUser(dispatch, id);
+  }, [dispatch, id]);
+
+  const { data: user, loading, error } = state.user;
+
+  if (loading) return <div>로딩중</div>;
   if (error) return <div>에러가 발생</div>;
   if (!user) return null;
 
